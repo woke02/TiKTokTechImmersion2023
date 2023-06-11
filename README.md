9ecc2232e76e9af449028c6d346f90e10644306b
woke02 <justwjyk@gmail.com>
2023-06-11 11:17:36 +0800

    assignment completed!

---
+++ README.md
@@ -0,0 +1,12 @@
+# assignment_demo_2023
+
+![Tests](https://github.com/TikTokTechImmersion/assignment_demo_2023/actions/workflows/test.yml/badge.svg)
+
+This is the submission for the TikTok Tech Immersion Program 2023 Backend Server Assignment. 
+
+
+## Pre-Requisites
+1. Go programming language 
+2. Docker Desktop 
+
+To build this program on your local Docker Desktop, right click on the docker-compose.yml file and select Compose. Thereafter, you can make HTTP request by calling POST localhost:8080/api/send and GET localhost:8080/api/pull using curl/Postman 
---
+++ rpc-server/redis.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	cli *redis.Client
+}
+
+func (c *RedisClient) InitClient(ctx context.Context, address, password string) error {
+
+	r := redis.NewClient(&redis.Options{
+		Addr:     address,
+		Password: password,
+		DB:       0,
+	})
+
+	if err := r.Ping(ctx).Err(); err != nil {
+		return err
+	}
+
+	c.cli = r
+	return nil
+}
+
+func (c *RedisClient) SaveMessage(ctx context.Context, roomID string, message *Message) error {
+	// Store in JSON
+	text, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	member := &redis.Z{
+		Score:  float64(message.Timestamp),
+		Member: text,
+	}
+
+	_, err = c.cli.ZAdd(ctx, roomID, *member).Result()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *RedisClient) GetMessagesByRoomID(ctx context.Context, roomID string, start, end int64, reverse bool) ([]*Message, error) {
+	var (
+		rawMessages []string
+		messages    []*Message
+		err         error
+	)
+
+	if reverse {
+		rawMessages, err = c.cli.ZRevRange(ctx, roomID, start, end).Result()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		rawMessages, err = c.cli.ZRange(ctx, roomID, start, end).Result()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for _, msg := range rawMessages {
+		temp := &Message{}
+		err := json.Unmarshal([]byte(msg), temp)
+		if err != nil {
+			return nil, err
+		}
+		messages = append(messages, temp)
+	}
+
+	return messages, nil
+}
